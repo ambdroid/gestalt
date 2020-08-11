3836d3ea6d3ee1c1887692f6f76febff31388773
ambdroid <61042504+ambdroid@users.noreply.github.com>
2020-08-11 02:40:53 -0400

    added autoproxy, purging; cleaned up

--- gestalt.py
+++ gestalt.py
@@ -1,197 +1,230 @@
 #!/usr/bin/python3.7
 
+from unicodedata import lookup as emojilookup
+import asyncio
+import time
+import math
+
 import sqlite3 as sqlite
 import discord
-
-import unicodedata
 
 import auth
 
 
-# for incoming reactions
-REACT_QUESTION = b'\\N{BLACK QUESTION MARK ORNAMENT}'
-REACT_DELETE = b'\\N{CROSS MARK}'
+REACT_QUERY = emojilookup("BLACK QUESTION MARK ORNAMENT")
+REACT_DELETE = emojilookup("CROSS MARK")
+# originally "BALLOT BOX WITH CHECK"
+# but this has visibility issues on ultradark theme
+REACT_CONFIRM = emojilookup("WHITE HEAVY CHECK MARK")
 
-# for outgoing reacting
-EMOJI_CONFIRM = unicodedata.lookup("BALLOT BOX WITH CHECK")
+COMMAND_PREFIX = "gs;"
+MAX_FILE_SIZE = 8*1024*1024
+DEFAULT_PREFIX = "g "
 
-GLOBAL_PREFIX = 'gs;'
-MAX_FILE_SIZE = 8388119
-DEFAULT_PREFIX = [(0, "g "), (0, "G ")]
+PURGE_AGE = 3600*24*7   # 1 week
+PURGE_TIMEOUT = 3600*2  # 2 hours
+
+HELPMSG = ("By default, I will proxy any message that begins "
+        "with `g ` or `G `. So `g hello!` will become `hello!`\n"
+        "\n"
+        "Use `" + COMMAND_PREFIX + "prefix` to set a custom prefix.\n"
+        "Examples:\n"
+        "`prefix =` lets you proxy with `=hello!`\n"
+        "`prefix \"h \"` lets you proxy with `h hello!`\n"
+        "`prefix delete` lets you revert your prefix to the defaults.\n"
+        "\n"
+        "Use `" + COMMAND_PREFIX + "auto` for autoproxy. "
+        "While autoproxy is on, I will proxy all your messages *except* those "
+        "that are prefixed.\n"
+        "Use `auto on` and `auto off` or just `auto` to toggle.\n"
+        "\n"
+        "Use`" + COMMAND_PREFIX + "nick` to change my nick.\n"
+        "\n"
+        "React with :x: to delete a message you sent.\n"
+        "React with :question: to query who sent a message.\n"
+        "(If you don't receive the DM, DM me first.)\n"
+        "\n"
+        "One last thing: if you upload a photo and put `spoiler` "
+        "somewhere in the message body, I'll spoiler it for you.\n"
+        "This is useful if you're on mobile.")
+
+AUTO_KEYWORDS = {
+        "on": 1,
+        "off": 0,
+        "yes": 1,
+        "no": 0
+}
+
+
+
+def begins(text, prefix):
+    return len(prefix) if text[:len(prefix)] == prefix else 0
+
 
 def is_text(message):
     return message.channel.type == discord.ChannelType.text
 
+
 def is_dm(message):
     return message.channel.type == discord.ChannelType.private
+
 
 def is_admin(message):
     return message.author.permissions_in(message.channel).administrator
 
-# returns False or message with prefix chopped
-def prefixed(message):
-    cur.execute("select hookid, prefix from users where userid = ?",
-            (message.author.id,))
-    rows = cur.fetchall()
-    if len(rows) == 0:
-        rows = DEFAULT_PREFIX
-    for i in rows:
-        if message.content[:len(i[1])] == i[1]:
-            return message.content[len(i[1]):]
-    return False
 
 class Gestalt(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loop.create_task(self.purge_loop())
+
+
+    async def purge_loop(self):
+        # this is purely a db task, no need to wait until ready
+        while not self.is_closed():
+            # time.time() and PURGE_AGE in seconds, snowflake timestamp in ms
+            # https://discord.com/developers/docs/reference#snowflakes
+            maxid = math.floor(1000*(time.time()-PURGE_AGE)-1420070400000)<<22
+            cur.execute("delete from history where deleted = 1 and msgid < ?",
+                    (maxid,))
+            conn.commit()
+            await asyncio.sleep(PURGE_TIMEOUT)
+
 
     async def on_ready(self):
         print('Logged in as {0}, id {1}!'.format(self.user, self.user.id))
         await self.change_presence(status = discord.Status.online,
-                activity = discord.Game(name = GLOBAL_PREFIX + "help"))
+                activity = discord.Game(name = COMMAND_PREFIX + "help"))
+
 
     async def do_command(self, message, cmd):
-        if (cmd == "shutdown" and is_admin(message)):
-            if not is_dm(message):
-                await message.delete()
-            await self.close()
-        elif (cmd == "commit" and is_admin(message)):
-            conn.commit()
-            print("Manual commit.")
-            await message.add_reaction(EMOJI_CONFIRM)
-        elif cmd == "help":
-            msgid = (await message.channel.send(
-                    "By default, I will proxy any message that begins " +
-                    "with `g ` or `G `. So `g hello!` will become `hello!`\n"+
-                    "(But I'll leave `gello` alone, of course.)\n"+
-                    "\n"+
-                    "Use `"+GLOBAL_PREFIX+"prefix` to set a custom prefix.\n"+
-                    "Examples:\n"+
-                    "`prefix =` to proxy with `=hello!`\n"+
-                    "`prefix \"h \"` to proxy with `h hello!`\n"+
-                    "`prefix delete` to revert your prefix to the defaults.\n"+
-                    "\n"+
-                    "You can change my nick with `"+GLOBAL_PREFIX+"nick`!\n"+
-                    "\n"+
-                    "React with :x: to delete a message you sent.\n"+
-                    "React with :question: to query who sent a message.\n"+
-                    "(If you don't receive the DM, DM me first.)\n"+
-                    "\n"+
-                    "One last thing: if you upload a photo and put `spoiler` "+
-                    "somewhere in the message body, I'll spoiler it for you.\n"+
-                    "This is useful if you're on mobile."
-                    )).id
+        # add an empty string to take place of arg if none given
+        arg = (cmd.split(maxsplit=1)+[""])[1]
+        if begins(cmd, "help"):
+            msgid = (await message.channel.send(HELPMSG)).id
             if is_text(message):
                 # put this in the history db so it can be deleted if desired
-                author = message.author
-                authid = author.id
-                authname = author.name + "#" + author.discriminator
-                cur.execute("insert into history values (?, ?, ?, NULL, 0)",
-                        (msgid, authid, authname))
+                cur.execute("insert into history values (?, 0, ?, '', '', 0)",
+                        (msgid, message.author.id))
 
+        elif begins(cmd, "prefix") and arg not in ["", '""']:
+            if arg[0] == '"' and arg[-1] == '"':
+                arg = arg[1:-1]
 
-        # discord trims whitespace so if there's any it isn't the whole message
-        elif cmd[:7] == "prefix ":
-            prefix = cmd[7:].strip();
-            if prefix[0] == '"' and prefix[-1] == '"':
-                prefix = prefix[1:-1]
+            if arg == "delete":
+                arg = None
+
+            cur.execute("update users set prefix = ? where userid = ?",
+                    (arg, message.author.id))
+
+            conn.commit()
+            await message.add_reaction(REACT_CONFIRM)
+
+        elif begins(cmd, "auto"):
             userid = message.author.id
-            if prefix == "delete":
-                cur.execute("delete from users where userid = ?", (userid,))
+            if arg == "":
+                cur.execute("update users set auto = (1-auto) where userid = ?",
+                        (userid,))
             else:
-                cur.execute("insert into users values (?, null, null, ?)"+
-                        "on conflict(userid) do update set prefix = ?",
-                        (userid, prefix, prefix))
-            await message.add_reaction(EMOJI_CONFIRM)
+                if arg not in AUTO_KEYWORDS:
+                    return
+                cur.execute("update users set auto = ? where userid = ?",
+                        (AUTO_KEYWORDS[arg], userid))
+            conn.commit()
+            await message.add_reaction(REACT_CONFIRM)
 
-        elif cmd[:5] == "nick ":
+        elif begins(cmd, "nick"):
             if is_dm(message):
                 await message.author.send("I can only do that in a server!");
                 return
-            await message.guild.get_member(self.user.id).edit(
-                    nick = cmd[5:].strip())
-            await message.add_reaction(EMOJI_CONFIRM)
+            try:
+                await message.guild.get_member(self.user.id).edit(nick = arg)
+                await message.add_reaction(REACT_CONFIRM)
+            except:
+                pass
 
 
     async def on_message(self, message):
-        if message.author.bot:
+        if message.author.bot or message.is_system():
             return
 
-        if not is_text(message):
-            if is_dm(message):
-                if prefixed(message):
-                    await message.channel.send(
-                            "I can't function in DMs :( I'm flattered though!")
-                    return
+        cur.execute("insert or ignore into users values (?, NULL, 0)",
+                (message.author.id,))
 
-                # in dms, global prefix is optional
-                cmd = message.content
-                if cmd[:len(GLOBAL_PREFIX)] == GLOBAL_PREFIX:
-                    cmd = cmd[len(GLOBAL_PREFIX):]
-                await self.do_command(message, cmd)
-                
+        # end of prefix or 0
+        offset = begins(message.content.lower(), COMMAND_PREFIX)
+        # command prefix is optional in DMs
+        if offset != 0 or is_dm(message):
+            await self.do_command(message, message.content[offset:])
             return
 
-        unprefixed = prefixed(message)
-        if unprefixed:
-            # time to proxy!
+        # guaranteed to exist due to above
+        row = cur.execute("select prefix, auto from users where userid = ?",
+                (message.author.id,)).fetchone()
+
+        # if no prefix set, use default
+        prefix = DEFAULT_PREFIX if row[0] == None else row[0]
+        offset = begins(message.content.lower(), prefix)
+
+        # don't proxy if:
+        # - auto off and not prefixed
+        # - auto on and prefixed
+        auto = row[1]
+        if not (offset == 0) == (auto == 0):
+            proxy = message.content[offset:].strip()
             msgfile = None
             if (len(message.attachments) > 0
                     and message.attachments[0].size <= MAX_FILE_SIZE):
-                attach = message.attachments[0]
                 # only copy the first attachment
-                msgfile = await attach.to_file()
-                # bonus feature: lets mobile users upload with spoilers
-                if unprefixed.lower().find("spoiler") != -1:
+                msgfile = await message.attachments[0].to_file()
+                # lets mobile users upload with spoilers
+                if proxy.lower().find("spoiler") != -1:
                     msgfile.filename = "SPOILER_" + msgfile.filename
 
-            msgid = (await message.channel.send(content = unprefixed,
+            msgid = (await message.channel.send(content = proxy,
                 file = msgfile)).id
             authid = message.author.id
+            chanid = message.channel.id
             authname = message.author.name + "#" + message.author.discriminator
-            cur.execute("insert into history values (?, ?, ?, ?, 0)",
-                    (msgid, authid, authname, unprefixed)) # deleted = 0
+            cur.execute("insert into history values (?, ?, ?, ?, ?, 0)",
+                    (msgid, chanid, authid, authname, proxy)) # deleted = 0
             await message.delete()
 
-        if message.content[:len(GLOBAL_PREFIX)] == GLOBAL_PREFIX:
-            await self.do_command(message, message.content[len(GLOBAL_PREFIX):])
 
     # on_reaction_add doesn't catch everything
     async def on_raw_reaction_add(self, payload):
         # first, make sure this is one of ours
-        # db is faster than fetch
-        cur.execute(
+        row = cur.execute(
             "select authid, authname from history where msgid = ? limit 1",
-            (payload.message_id,))
-        row = cur.fetchone()
+            (payload.message_id,)).fetchone()
         if row == None:
             return
 
-        # emoji name given in unicode, so translate to ascii
-        emoji = payload.emoji.name.encode(encoding = 'ascii',
-                errors = 'namereplace')
-
-        # we can't fetch the message directly, at least not in this api binding.
-        # so fetch the channel first, *then* use it to fetch the message.
+        # we can't fetch the message directly
+        # so fetch the channel first, and use *that* to fetch the message.
         channel = self.get_channel(payload.channel_id)
         reactor = self.get_user(payload.user_id)
         message = await channel.fetch_message(payload.message_id)
 
         if message.author.id != self.user.id:
-            return
+            raise ValueError("Something is terribly wrong.")
 
-        if emoji == REACT_QUESTION:
+        emoji = payload.emoji.name
+        if emoji == REACT_QUERY:
             try:
                 await reactor.send(
                         "Message sent by " + row[1] + " id " + str(row[0]))
             except discord.Forbidden:
                 pass
-
             await message.remove_reaction(payload.emoji.name, reactor)
 
         elif emoji == REACT_DELETE:
             # only sender may delete proxied message
             if payload.user_id == row[0]:
                 cur.execute(
-                    "update history set deleted = 1 where msgid = ?",
-                    (payload.message_id,))
+                        "update history set deleted = 1,"
+                        "authname = '' where msgid = ?",
+                        (payload.message_id,))
                 await message.delete()
             else:
                 await message.remove_reaction(payload.emoji.name, reactor)
@@ -203,18 +236,19 @@ instance = Gestalt()
 conn = sqlite.connect("gestalt.db")
 cur = conn.cursor()
 cur.execute(
-	"create table if not exists history("+
-        "msgid integer primary key,"+
-        "authid integer,"+
-        "authname text,"+
+	"create table if not exists history("
+        "msgid integer primary key,"
+        "chanid integer,"
+        "authid integer,"
+        "authname text,"
         "content text,"
         "deleted integer)")
 cur.execute(
-	"create table if not exists users("+
-        "userid integer primary key,"+
-        "guildid integer,"+ # currently unused
-        "hookid integer,"+  # currently unused
-        "prefix text)")
+	"create table if not exists users("
+        "userid integer primary key,"
+        "prefix text,"
+        "auto integer)")
+cur.execute("pragma secure_delete")
 
 try:
     instance.run(auth.token)
