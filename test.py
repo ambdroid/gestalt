1ba3bbcfa05b901d6ea861c826a92b8e44108fcc
ambdroid <ambreslau@gmail.com>
2020-08-15 17:49:49 -0400

    group actions together for speed improvement

--- test.py
+++ test.py
@@ -11,9 +11,13 @@ import auth
 TIMEOUT = 0.5
 
 class TestClient(discord.Client):
-    def  __init__(self, action, content, waitfor):
+    def  __init__(self, actions):
         super().__init__(fetch_offline_members = False)
-        self.loop.create_task(self.do_action(action, content, waitfor))
+        self.loop.create_task(self.do_actions(actions))
+
+    async def do_actions(self, actions):
+        self.waited =  tuple([(await self.do_action(*x)) for x in actions])
+        await self.close()
 
     async def do_action(self, action, content, waitfor):
         await self.wait_until_ready()
@@ -25,24 +29,24 @@ class TestClient(discord.Client):
             await message.add_reaction(emojilookup(content[1]))
         if waitfor:
             try:
-                self.waited = await self.wait_for(waitfor, timeout = TIMEOUT)
+                return await self.wait_for(waitfor, timeout = TIMEOUT)
             except: # timeout
-                self.waited = None
-        else:
-            self.waited = None
-        await self.close()
+                pass
+        return None
 
     def run(self, token):
         super().run(token)
         return self.waited
 
-def test(bot, action, content, waitfor):
-    client = TestClient(action, content, waitfor)
-    ret = client.run(auth.bots[bot])
+def test(bot, actions):
+    ret = TestClient(actions).run(auth.bots[bot])
     asyncio.set_event_loop(asyncio.new_event_loop())
     return ret
 
 
-print(test(0, "message", "g hello, world!", "message"))
-print(test(0, "react", (1, "BLACK QUESTION MARK ORNAMENT"), "message"))
-print(test(0, "react", (2, "CROSS MARK"), "raw_message_delete"))
+print(test(0,(
+    ("message", "g hello, world!", "message"),
+    ("react", (1, "BLACK QUESTION MARK ORNAMENT"), "message"),
+    ("react", (2, "CROSS MARK"), "raw_message_delete"))))
+test(0,(
+    ("message", "test", None),))
