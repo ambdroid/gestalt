9d6dda4a29dc55f70d9eb6b48db7056a201e47bf
ambdroid <61042504+ambdroid@users.noreply.github.com>
2020-10-06 19:07:54 -0400

    more readable tests

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ class Object:
 
 # for simplicity, all Users are Members, the same across all guilds
 class Member(Object):
+    users = {}
     def __init__(self, **kwargs):
         self.bot = False
         self.dm_channel = None
@@ -33,6 +34,7 @@ class Member(Object):
         super().__init__(**kwargs)
         self.display_name = self.nick = self.name
         # self.mention = "<@!%d>" % self.id
+        Member.users[self.id] = self
     def __str__(self):
         return self.name + "#" + self.discriminator
     def avatar_url_as(self, **kwargs):
@@ -40,7 +42,7 @@ class Member(Object):
     async def send(self, content = None, embed = None, file = None):
         if self.dm_channel == None:
             self.dm_channel = Channel(type = discord.ChannelType.private,
-                    members = [self, user[0]])
+                    members = [self, bot])
         await self.dm_channel.send(
                 content = content, embed = embed, file = file)
 
@@ -58,8 +60,7 @@ class Message(Object):
         if self.content != None:
             # mentions can also be in the embed but that's irrelevant here
             for mention in re.findall("(?<=\<\@\!)[0-9]+(?=\>)", self.content):
-                self.mentions.append(
-                        user[[x.id for x in user].index(int(mention))])
+                self.mentions.append(Member.users[int(mention)])
     async def delete(self):
         self.channel._messages.remove(self)
         self._deleted = True
@@ -77,7 +78,7 @@ class Message(Object):
                     emoji = discord.PartialEmoji(name = emoji),
                     event_type = None))
     async def add_reaction(self, emoji):
-        await self._react(emoji, user[0])
+        await self._react(emoji, bot)
     async def remove_reaction(self, emoji, member):
         del self.reactions[[x.emoji for x in self.reactions].index(emoji)]
 
@@ -94,17 +95,19 @@ class Webhook(Object):
         # keep author None because it's weird with webhook messages
         msg = Message(**kwargs) # note: absorbs other irrelevant arguments
         msg.webhook_id = self.id
-        msg.author = user[0] # so on_message doesn't complain about no author
+        msg.author = bot # so on_message doesn't complain about no author
         await self._channel._add(msg)
         return msg
 
 class Channel(Object):
+    channels = {}
     def __init__(self, **kwargs):
         self._messages = []
         self.members = []
         self.type = discord.ChannelType.text
         super().__init__(**kwargs)
         self.guild = Guild() # FIXME when guilds need fleshing out
+        Channel.channels[self.id] = self
     async def _add(self, msg):
         msg.channel = self
         self._messages.append(msg)
@@ -114,24 +117,24 @@ class Channel(Object):
     async def fetch_message(self, id):
         return self._messages[[x.id for x in self._messages].index(id)]
     async def send(self, content = None, embed = None, file = None):
-        msg = Message(author = user[0], content = content, embed = embed)
+        msg = Message(author = bot, content = content, embed = embed)
         await self._add(msg)
         return msg
 
 class Guild:
     def get_member(self, user_id):
-        return user[[x.id for x in user].index(user_id)]
+        return Member.users[user_id]
 
 class TestBot(gestalt.Gestalt):
     def __del__(self):
         pass # suppress "closing database" message
     @property
     def user(self):
-        return user[0]
+        return bot
     def get_user(self, id):
-        return user[[x.id for x in user].index(id)]
+        return Member.users[id]
     def get_channel(self, id):
-        return chan[[x.id for x in chan].index(id)]
+        return Channel.channels[id]
 
 def send(user, channel, contents):
     for x in contents:
@@ -151,16 +154,16 @@ class GestaltTest(unittest.TestCase):
         discord.AsyncWebhookAdapter.__init__ = lambda self, adapter : None
         discord.Webhook.partial = Webhook.partial
 
-        msgs = send(user[1], chan[0], [
-            "gs;swap <@!%d>" % user[2].id,
+        msgs = send(alpha, chan, [
+            "gs;swap <@!%d>" % beta.id,
             "g no swap"])
 
         self.assertReacted(msgs[0])
         self.assertIsNone(msgs[1].webhook_id)
 
-        msgs = send(user[2], chan[0], [
+        msgs = send(beta, chan, [
             "g no swap",
-            "gs;swap <@!%d>" % user[1].id,
+            "gs;swap <@!%d>" % alpha.id,
             "g swap",
             "gs;swap off",
             "g no swap",
@@ -172,16 +175,16 @@ class GestaltTest(unittest.TestCase):
         for i in [0, 4]:
             self.assertIsNone(msgs[i].webhook_id)
 
-        msgs = send(user[1], chan[0], [
+        msgs = send(alpha, chan, [
             "g no swap",
-            "gs;swap <@!%d>" % user[2].id,
+            "gs;swap <@!%d>" % beta.id,
             "g swap"])
 
         self.assertIsNone(msgs[0].webhook_id)
         self.assertReacted(msgs[1])
         self.assertIsNotNone(msgs[2].webhook_id)
 
-        msgs = send(user[2], chan[0], [
+        msgs = send(beta, chan, [
             "g swap",
             "gs;swap off",
             "g no swap"])
@@ -190,19 +193,19 @@ class GestaltTest(unittest.TestCase):
         self.assertReacted(msgs[1])
         self.assertIsNone(msgs[2].webhook_id)
 
-        msgs = send(user[1], chan[0], ["g no swap"])
+        msgs = send(alpha, chan, ["g no swap"])
         self.assertIsNone(msgs[0].webhook_id)
 
     def test_help(self):
-        msg = send(user[1], chan[0], ["gs;help"])[0]
-        self.assertIsNotNone(chan[0]._messages[-1].embed)
+        msg = send(alpha, chan, ["gs;help"])[0]
+        self.assertIsNotNone(chan._messages[-1].embed)
         self.assertReacted(msg, gestalt.REACT_DELETE)
-        run(msg._react(gestalt.REACT_DELETE, user[1]))
+        run(msg._react(gestalt.REACT_DELETE, alpha))
         self.assertTrue(msg._deleted)
 
     def test_prefix_auto(self):
         # test every combo of auto, prefix, and also the switches thereof
-        msgs = send(user[1], chan[0],[
+        msgs = send(alpha, chan,[
             "no prefix, no auto",
             "g default prefix",
             "gs;prefix =",
@@ -221,31 +224,29 @@ class GestaltTest(unittest.TestCase):
             self.assertReacted(msgs[i])
         for i in [0, 5, 11]:
             self.assertEqual(len(msgs[i].reactions), 0)
-            self.assertEqual(msgs[i].author, user[1]) # message not proxied
+            self.assertEqual(msgs[i].author, alpha) # message not proxied
         for i in [1, 3, 6, 9]:
-            self.assertEqual(msgs[i].author, user[0]) # message proxied
+            self.assertEqual(msgs[i].author, bot) # message proxied
 
     def test_query_delete(self):
-        msg = send(user[1], chan[0], ["g reaction test"])[0]
-        run(msg._react(gestalt.REACT_QUERY, user[2]))
+        msg = send(alpha, chan, ["g reaction test"])[0]
+        run(msg._react(gestalt.REACT_QUERY, beta))
         self.assertNotEqual(
-                user[2].dm_channel._messages[-1].content.find(user[1].name), -1)
+                beta.dm_channel._messages[-1].content.find(alpha.name), -1)
 
-        run(msg._react(gestalt.REACT_DELETE, user[2]))
+        run(msg._react(gestalt.REACT_DELETE, beta))
         self.assertEqual(len(msg.reactions), 0)
         self.assertFalse(msg._deleted)
 
-        run(msg._react(gestalt.REACT_DELETE, user[1]))
+        run(msg._react(gestalt.REACT_DELETE, alpha))
         self.assertTrue(msg._deleted)
 
 
 if __name__ == "__main__":
-    user = [
-            Member(name = "Gestalt", bot = True),
-            Member(name = "test-1"),
-            Member(name = "test-2")
-            ]
-    chan = [Channel()]
+    bot = Member(name = "Gestalt", bot = True)
+    alpha = Member(name = "test-alpha")
+    beta = Member(name = "test-beta")
+    chan = Channel()
 
     instance = TestBot(dbfile = ":memory:", purge = False)
     if unittest.main(exit = False).result.wasSuccessful():
