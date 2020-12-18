9e25f0ce6f0137213d05c44beeb3fbed932b1af7
ambdroid <61042504+ambdroid@users.noreply.github.com>
2020-12-18 02:14:27 -0500

    moved do_command() to commands.py

---
+++ defs.py
@@ -0,0 +1,61 @@
+from functools import reduce
+import enum
+
+import discord
+
+from config import *
+
+
+INTENTS = discord.Intents(
+        guilds = True,
+        members = True,
+        messages = True,
+        reactions = True)
+
+
+PERMS = discord.permissions.Permissions(
+        add_reactions = True,
+        read_messages = True,
+        send_messages = True,
+        manage_messages = True,
+        embed_links = True,
+        attach_files = True,
+        use_external_emojis = True,
+        manage_webhooks = True)
+
+
+# limits for non-Nitro users by boost level
+MAX_FILE_SIZE = [
+        8*1024*1024,
+        8*1024*1024,
+        50*1024*1024,
+        100*1024*1024]
+
+
+@enum.unique
+class Prefs(enum.IntFlag):
+#   auto        = 1 << 0
+    replace     = 1 << 1
+#   autoswap    = 1 << 2
+    errors      = 1 << 3
+    delay       = 1 << 4
+
+
+@enum.unique
+class ProxyType(enum.IntEnum):
+    override    = 0
+    collective  = 1
+    swap        = 2
+
+
+DEFAULT_PREFS = reduce(lambda a, b : a | Prefs[b], DEFAULT_PREFS, 0)
+REPLACEMENTS = [(re.compile(x, re.IGNORECASE), y) for x, y in REPLACEMENTS]
+
+
+def is_text(message):
+    return message.channel.type == discord.ChannelType.text
+
+
+def is_dm(message):
+    return message.channel.type == discord.ChannelType.private
+
