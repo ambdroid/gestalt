6a40ca400289af3e84b61b0d5e769af4f79e36e5
ambdroid <61042504+ambdroid@users.noreply.github.com>
2025-06-08 21:21:54 -0400

    fix schema update

--- schema/24.sql
+++ schema/24.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION ;
-delete from users where (select count() from proxies where proxies.userid == users.userid) == 1 and not exists (select 1 from history where history.authid = users.userid);
+delete from users where not exists (select 1 from history where history.authid = users.userid);
 delete from members where not exists (select 1 from users where users.userid = members.userid);
-delete from taken where exists(select 1 from proxies where proxies.proxid == taken.id and not exists (select 1 from users where users.userid = proxies.userid));
-delete from proxies where not exists (select 1 from users where users.userid = proxies.userid);
+delete from taken where exists(select 1 from proxies where (proxies.proxid, type) == (taken.id, 0) and not exists (select 1 from users where users.userid = proxies.userid));
+delete from proxies where type == 0 and not exists (select 1 from users where users.userid = proxies.userid);
 COMMIT TRANSACTION ;
